89571d43f58b9827d5b8d9f9ab8e4989551ec99f
Nayla Gomes <nayla.cgs@gmail.com>
2023-05-23 11:35:43 -0300

    setup

--- web/src/app/app.component.spec.ts
+++ web/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'poc-angular-express'`, () => {
+  it(`should have as title 'web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('poc-angular-express');
+    expect(app.title).toEqual('web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('poc-angular-express app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('web app is running!');
   });
 });
